29048dc9d1406148e873728796d1054d98096d81
dennismutitu006 <dennismutitu06@gmail.com>
2024-05-30 15:17:07 -0700

    For privacy reasons, when cloneCar is called on a EVCar object, the object returned should be an instance of Car instead of EVCar.

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,22 @@
+import Car from './10-car';
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this.range = range;
+  }
+
+  get range() {
+    return this._range;
+  }
+
+  set range(range) {
+    this._range = range;
+  }
+
+  cloneCar() {
+    const Make = super.constructor[Symbol.make];
+
+    return new Make();
+  }
+}
