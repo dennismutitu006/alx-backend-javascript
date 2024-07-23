d0bfd10596789c324c83d530181039235d1121c8
dennismutitu <dennismutitu06@gmail.com>
2024-07-23 14:10:57 +0300

    trying to debug the error on stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,7 +1,6 @@
-// 1-stdin.js
-// Print the welcome message
-console.log('Welcome to Holberton School, what is your name?');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 // Read input from stdin
+process.stdin.setEncoding('utf-8');
 process.stdin.on('data', (data) => {
   const name = data.trim();
   console.log(`Your name is: ${name}`);
