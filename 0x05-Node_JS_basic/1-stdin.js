70a714d4786b9b9a73e2780b7fc811868151379f
dennismutitu <dennismutitu06@gmail.com>
2024-07-23 13:56:19 +0300

    using process stdin

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,11 @@
+// 1-stdin.js
+// Print the welcome message
+console.log('Welcome to Holberton School, what is your name?');
+// Read input from stdin
+process.stdin.setEncoding('utf-8');
+process.stdin.on('data', (data) => {
+  const name = data.trim();
+  console.log(`Your name is: ${name}`);
+  console.log('This important software is now closing');
+  process.exit();
+});
