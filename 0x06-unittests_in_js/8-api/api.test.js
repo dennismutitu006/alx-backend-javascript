58a116592abc14b56c2d1b61e8ad11b4f463596d
dennismutitu <dennismutitu06@gmail.com>
2024-07-26 01:32:16 +0300

    update

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,32 +1,19 @@
-// 8-api/api.test.js
 const request = require('request');
 const { expect } = require('chai');
-const app = require('./api'); // Import the Express app
-const port = 7865;
 
-describe('Index page', () => {
-  before((done) => {
-    app.listen(port, () => {
-      console.log(`Test server running on port ${port}`);
-      done();
+describe('Integration Testing', () => {
+  describe('GET /', () => {
+    it('Code: 200 | Body: Welcome to the payment system', (done) => {
+      const options = {
+        url: 'http://localhost:7865',
+        method: 'GET',
+      };
+
+      request(options, function (error, response, body) {
+        expect(response.statusCode).to.equal(200);
+        expect(body).to.equal('Welcome to the payment system');
+        done();
+      });
     });
-  });
-
-  it('should return status code 200', (done) => {
-    request.get(`http://localhost:${port}`, (err, res, body) => {
-      expect(res.statusCode).to.equal(200);
-      done();
-    });
-  });
-
-  it('should return correct message', (done) => {
-    request.get(`http://localhost:${port}`, (err, res, body) => {
-      expect(body).to.equal('Welcome to the payment system');
-      done();
-    });
-  });
-
-  after((done) => {
-    app.close(done);
   });
 });
